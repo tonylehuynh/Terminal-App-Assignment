7b9723277ec7f750010086272c022ed955cbd5db
tonylehuynh <tonylehuynh66@gmail.com>
2022-12-15 17:15:57 +1000

    Created initial while loop for user input options

--- app.py
+++ app.py
@@ -14,5 +14,14 @@ def main_menu():
     print('3. Exit')
     while True:
         option = int(input('\nChoose an option:\n'))
+        if option == 3:
+            print('Exiting the program. Have a nice day!')
+            break
+        elif option == 1:
+            print('1')
+        elif option == 2:
+            print('2')
+        else:
+            print('Invalid input. Please enter 1, 2 or 3')
 
 main_menu()
