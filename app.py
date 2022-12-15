814451e446d239f6b99e5a6b2569400e6f757e43
tonylehuynh <tonylehuynh66@gmail.com>
2022-12-15 19:13:02 +1000

    Fixed while loop

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ Description of python file
 """
 
 import os
-
+from budget_projection import budget_projection
 
 def main_menu():
     os.system('cls')
@@ -13,24 +13,15 @@ def main_menu():
     print('2. Track your budget')
     print('3. Exit')
     while True:
-        try: 
-            option = int(input('\nChoose an option:\n'))
-            if option == 3:
-                print('Exiting the program. Have a nice day!')
-                break
-            elif option == 1:
-                print('1')
-            elif option == 2:
-                print('2')
-            else:
-                print('Invalid input... Please enter 1, 2 or 3 for the following options:\n')
-                print('1. Budget projection')
-                print('2. Track your budget')
-                print('3. Exit')
-        except ValueError:
-            print('Invalid input... Please enter 1, 2 or 3 for the following options:\n')
-            print('1. Budget projection')
-            print('2. Track your budget')
-            print('3. Exit')
+        option = (input('\nChoose an option:\n'))
+        if option == "3":
+            print('Exiting the program. Have a nice day!')
+            break
+        elif option == "1":
+            budget_projection()
+        elif option == "2":
+            pass
+        else:
+            print('INVALID INPUT... Please enter 1, 2 or 3:')
 
 main_menu()
---
+++ budget_projection.py
@@ -0,0 +1,3 @@
+
+def budget_projection():
+    pass
