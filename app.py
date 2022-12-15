58595fba0cd33f2437532214157d9e3bfdc3bc31
tonylehuynh <tonylehuynh66@gmail.com>
2022-12-15 17:07:40 +1000

    Created main_menu function with print statements displaying options for user

--- app.py
+++ app.py
@@ -2,7 +2,17 @@
 Description of python file
 """
 
-a = 10
-b = 30
-sum = a + b
-print(sum)
+import os
+
+
+def main_menu():
+    os.system('cls')
+    print('Welcome to your personal budget app!\n')
+    print('Please enter 1, 2 or 3 for the following options:\n')
+    print('1. Budget projection')
+    print('2. Track your budget')
+    print('3. Exit')
+    while True:
+        option = int(input('\nChoose an option:\n'))
+
+main_menu()
