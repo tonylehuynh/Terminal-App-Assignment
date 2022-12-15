e3fc093c9d243951f331c0ad3d937796ca01b8df
tonylehuynh <tonylehuynh66@gmail.com>
2022-12-15 19:31:14 +1000

    Wrote comms for the budget projection feature

--- budget_projection.py
+++ budget_projection.py
@@ -1,3 +1,15 @@
+import os
+
 
 def budget_projection():
-    pass
+    os.system("cls")
+    print("We will now project your budget for the next month & year!\n")
+    print("Don't worry about being exact with the numbers.")
+    print("This is simply a general estimate and projection of your budget.\n")
+
+def project_income():
+    
+
+
+budget_projection()
+project_income()
