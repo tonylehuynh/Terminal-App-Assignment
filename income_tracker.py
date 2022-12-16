6b791c13f74a22939c38bc5637279784111275c7
tonylehuynh <tonylehuynh66@gmail.com>
2022-12-16 13:50:59 +1000

    Wrote functionality to append income data to csv file

--- income_tracker.py
+++ income_tracker.py
@@ -6,9 +6,25 @@ import csv
 import datetime
 
 
-def file_does_not_exist_income():
-    with open("income.csv", "w", newline="") as csvfile:
+def file_does_not_exist():
+    with open("data.csv", "w", newline="") as csvfile:
         writer = csv.writer(csvfile, delimiter=",")
-        writer.writerow(['Date', 'Amount'])
+        writer.writerow(["Date", "Category", "Amount"])
 
+
+
+def income_tracker():
+    if not os.path.exists("data.csv"):
+        file_does_not_exist()
+    if os.path.exists("data.csv"):
+        with open("data.csv", "a", newline="") as csvfile:
+            writer = csv.writer(csvfile, delimiter=",")
+
+            category = "income"
+            date = "date"
+            amount = "amount"
+
+            writer.writerow([date,category,amount])
+
+income_tracker()
 
--- test_income_tracker.py
+++ test_income_tracker.py
@@ -1,20 +1,20 @@
 """Testing of the income tracker feature"""
 
 import os
-from income_tracker import file_does_not_exist_income
+from income_tracker import file_does_not_exist
 
 
 
 # Test that function will successfully create csv file
 def test_create_csv_file():
-    file_does_not_exist_income()
-    csv_file_exists = os.path.exists("income.csv")
+    file_does_not_exist()
+    csv_file_exists = os.path.exists("data.csv")
     # File should be created and return true
     assert csv_file_exists == True 
 
 # Case 2 - Ensure function outputs correct file format
 def test_file_format():
-    file_does_not_exist_income()
-    wrong_format = os.path.exists("income.txt")
+    file_does_not_exist()
+    wrong_format = os.path.exists("data.txt")
     # Should return false as file should only be in csv form
     assert wrong_format == False
