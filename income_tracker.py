43e2d5659bc9190fbd310df0005fe606c4b18d19
tonylehuynh <tonylehuynh66@gmail.com>
2022-12-16 14:40:55 +1000

    Created two test cases to test that function will correctly append user data into csv

--- income_tracker.py
+++ income_tracker.py
@@ -6,25 +6,38 @@ import csv
 import datetime
 
 
+def income_category():
+    return "income"
+
+
+def input_date():
+    pass
+
+
+def input_amount():
+    pass
+
+
 def file_does_not_exist():
     with open("data.csv", "w", newline="") as csvfile:
         writer = csv.writer(csvfile, delimiter=",")
         writer.writerow(["Date", "Category", "Amount"])
 
 
+def store_budget_data():
+    with open("data.csv", "a", newline="") as csvfile:
+        writer = csv.writer(csvfile, delimiter=",")
+
+        category = "income"
+        test_date = datetime.datetime(2022, 6, 6)
+        date = test_date
+        amount = 100.20
+
+        writer.writerow([date, category, amount])
+
 
 def income_tracker():
     if not os.path.exists("data.csv"):
         file_does_not_exist()
     if os.path.exists("data.csv"):
-        with open("data.csv", "a", newline="") as csvfile:
-            writer = csv.writer(csvfile, delimiter=",")
-
-            category = "income"
-            date = "date"
-            amount = "amount"
-
-            writer.writerow([date,category,amount])
-
-income_tracker()
-
+        store_budget_data()
--- test_income_tracker.py
+++ test_income_tracker.py
@@ -1,8 +1,9 @@
 """Testing of the income tracker feature"""
 
 import os
-from income_tracker import file_does_not_exist
-
+import csv
+import datetime
+from income_tracker import file_does_not_exist, store_budget_data
 
 
 # Test that function will successfully create csv file
@@ -10,7 +11,8 @@ def test_create_csv_file():
     file_does_not_exist()
     csv_file_exists = os.path.exists("data.csv")
     # File should be created and return true
-    assert csv_file_exists == True 
+    assert csv_file_exists == True
+
 
 # Case 2 - Ensure function outputs correct file format
 def test_file_format():
@@ -18,3 +20,25 @@ def test_file_format():
     wrong_format = os.path.exists("data.txt")
     # Should return false as file should only be in csv form
     assert wrong_format == False
+
+
+# Test that function will correctly append data to csv file
+# Case 1 - Function only appends string values into csv file
+def test_append_csv_string():
+    store_budget_data()
+    with open("data.csv", "r") as csv_file:
+        reader = csv.reader(csv_file)
+        rows = list(reader)
+        # Below returns true as the string values have been temporarily written in
+        assert rows[-1] == ["date", "income", "amount"]
+
+# Case 2 - Function instead appends a date & float value into csv file
+def test_append_csv_float_date():
+    store_budget_data()
+    with open("data.csv", "r") as csv_file:
+        reader = csv.reader(csv_file)
+        rows = list(reader)
+        test_date = str(datetime.datetime(2022, 6, 6))
+        test_float = str(100.20)
+        # Returns true. Function outputs date & float values however csv stores these as string values
+        assert rows[-1] == [test_date, "income", test_float]
