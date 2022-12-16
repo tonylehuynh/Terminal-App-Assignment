5b14d7bfc3dbc682710fb90a94a20090d2bd6d87
tonylehuynh <tonylehuynh66@gmail.com>
2022-12-16 17:19:34 +1000

    Function to return expense category from a dict after receving user input has been completed

--- expense_tracker.py
+++ expense_tracker.py
@@ -7,8 +7,40 @@ from income_tracker import input_date, input_amount, file_does_not_exist
 
 
 def expense_category():
-    
-
+    category = {
+        1: "Rent",
+        2: "Mortgage",
+        3: "Food",
+        4: "Transportation",
+        5: "Utilities",
+        6: "Internet",
+        7: "Phone",
+        8: "Insurance",
+        9: "Subscriptions",
+        10: "Clothing",
+        11: "Exercise",
+        12: "Socialising",
+        13: "Miscellaneous",
+    }
+    print("Here are the categories of expenses:\n")
+    print("1. Rent")
+    print("2. Mortgage")
+    print("3. Food")
+    print("4. Transportation")
+    print("5. Utilities")
+    print("6. Internet")
+    print("7. Phone")
+    print("8. Insurance")
+    print("9. Subscriptions")
+    print("10. Clothing")
+    print("11. Exercise")
+    print("12. Socialising")
+    print("13. Miscellaneous")
+    user_input = int(
+        input("\nEnter a number to choose your option (e.g. Type 1 for Rent):\n")
+    )
+    expense_type = category.get(user_input)
+    return expense_type
 
 
 
